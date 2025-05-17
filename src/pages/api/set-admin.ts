07e0c7cf559bc05d68acaedd6b7df9565fd28ef4
reinaldoper <reinaldoper83@gmail.com>
2025-05-17 02:02:05 -0400

    fix: Ajusta variaveis de ambiente

--- src/pages/api/set-admin.ts
+++ src/pages/api/set-admin.ts
@@ -5,16 +5,16 @@ import type { ServiceAccount } from "firebase-admin/app";
 
 const serviceAccount = {
   type: "service_account",
-  project_id: process.env.NEXT_PUBLIC_PROJECT_ID,
-  private_key_id: process.env.NEXT_PUBLIC_PRIVATE_KEY_ID,
-  private_key: process.env.NEXT_PUBLIC_PRIVATE_KEY?.replace(/\\n/g, '\n'),
-  client_email: process.env.NEXT_PUBLIC_CLIENT_EMAIL,
-  client_id: process.env.NEXT_PUBLIC_CLIENT_ID,
-  auth_uri: process.env.NEXT_PUBLIC_AUTH_URI,
-  token_uri: process.env.NEXT_PUBLIC_TOKEN_URI,
-  auth_provider_x509_cert_url: process.env.NEXT_PUBLIC_AUTH_PROVIDER_CERT_URL,
-  client_x509_cert_url: process.env.NEXT_PUBLIC_CLIENT_CERT_URL,
-  universe_domain: process.env.NEXT_PUBLIC_UNIVERSE_DOMAIN,
+  project_id: process.env.PROJECT_ID,
+  private_key_id: process.env.PRIVATE_KEY_ID,
+  private_key: process.env.PRIVATE_KEY?.replace(/\\n/g, '\n'),
+  client_email: process.env.CLIENT_EMAIL,
+  client_id: process.env.CLIENT_ID,
+  auth_uri: process.env.AUTH_URI,
+  token_uri: process.env.TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.AUTH_PROVIDER_CERT_URL,
+  client_x509_cert_url: process.env.CLIENT_CERT_URL,
+  universe_domain: process.env.UNIVERSE_DOMAIN,
 };
 
 
