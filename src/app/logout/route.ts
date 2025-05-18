8775a46802a0b0d020d81245fbcf592467f08b4a
reinaldoper <reinaldoper83@gmail.com>
2025-05-18 01:05:43 -0400

    fix: Ajusta função logout

--- src/app/logout/route.ts
+++ src/app/logout/route.ts
@@ -1,7 +1,9 @@
 
 import { NextResponse } from "next/server";
+import { logout } from "@/lib/auth";
 
 export async function POST() {
+  await logout();
   const response = NextResponse.redirect(new URL("/login", process.env.NEXT_PUBLIC_BASE_URL));
   response.cookies.set("__session", "", {
     maxAge: 0,
